3187b6464fc3f552976a52c4ed72cfc9e940345b
0x1306a94 <0x1306a94@gmail.com>
2023-01-06 17:08:30 +0800

    WIP

---
+++ include/sqlite-winq/binding/declare.h
@@ -0,0 +1,24 @@
+//
+//  declare.h
+//  mars
+//
+//  Created by king on 2023/1/6.
+//
+
+#ifndef sqlite_winq_binding_declare_h
+#define sqlite_winq_binding_declare_h
+
+namespace SQLITEWINQ {
+
+class Binding;
+class ColumnBinding;
+class ConstraintBindingBase;
+class ConstraintPrimaryKeyBinding;
+class ConstraintUniqueBinding;
+class ConstraintCheckBinding;
+class IndexBinding;
+
+};  //namespace SQLITEWINQ
+
+#endif /* sqlite_winq_binding_declare_h */
+
